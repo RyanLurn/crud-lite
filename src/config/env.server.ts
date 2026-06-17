c56fb7afc87f0cf4f8b6e8adeca1396171dff8ad
Ryan Luong <lumintriet@gmail.com>
2026-06-17 20:38:54 +0700

    feat(config/env): add SQLITE_FILE_PATH to the list of env vars.

--- src/config/env.server.ts
+++ src/config/env.server.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 const envValidator = z.object({
   NODE_ENV: z.enum(["development", "testing", "staging", "production"]),
+  SQLITE_FILE_PATH: z.string().min(1),
 });
 
 export const env = envValidator.parse(process.env);
