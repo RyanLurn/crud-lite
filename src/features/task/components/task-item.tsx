7f637be092eec2dad0fbea9274db69331d6b02ec
Ryan Luong <lumintriet@gmail.com>
2026-06-18 18:58:41 +0700

    feat(task/ui): implement the updateTaskStatus server function in the TaskItem component.

--- src/features/task/components/task-item.tsx
+++ src/features/task/components/task-item.tsx
@@ -1,25 +1,74 @@
-import type { ComponentProps } from "react";
-
 import { ClipboardPenLine, ClipboardCheck } from "lucide-react";
+import { type ComponentProps, useState } from "react";
+import { useRouter } from "@tanstack/react-router";
+import { toast } from "sonner";
 
 import type { SelectedTask } from "@/db/schema/tables/task";
+import type { StrictOmit } from "@/types/helpers";
 
+import { updateTaskStatus } from "@/features/task/operations/update-task-status";
 import { ItemContent, ItemMedia, ItemTitle, Item } from "@/components/ui/item";
+import { Spinner } from "@/components/ui/spinner";
 import { cn } from "@/lib/cn";
 
-interface TaskItemProps extends ComponentProps<typeof Item> {
+interface TaskItemProps extends StrictOmit<
+  ComponentProps<typeof Item>,
+  "onClick"
+> {
   task: SelectedTask;
 }
 
 export function TaskItem({
   task,
+  className,
   variant = "outline",
   ...props
 }: TaskItemProps) {
+  const router = useRouter();
+  const [isToggling, setIsToggling] = useState(false);
+
+  async function toggleStatus() {
+    setIsToggling(true);
+    const newStatus = task.status === "planned" ? "done" : "planned";
+    try {
+      const result = await updateTaskStatus({
+        data: {
+          id: task.id,
+          status: newStatus,
+        },
+      });
+      if (!result.success) {
+        toast.error(result.error.message);
+      } else {
+        await router.invalidate({ sync: true });
+      }
+    } catch (error) {
+      if (import.meta.env.DEV) {
+        console.error(error);
+      }
+      toast.error(
+        `Something went wrong while trying to mark ${task.name} as ${newStatus}.`
+      );
+    }
+    setIsToggling(false);
+  }
+
   return (
-    <Item variant={variant} {...props}>
+    <Item
+      className={cn(
+        isToggling
+          ? "cursor-progress text-muted-foreground"
+          : "cursor-pointer hover:bg-muted",
+        className
+      )}
+      onClick={() => void toggleStatus()}
+      variant={variant}
+      {...props}
+    >
       <ItemMedia variant="icon">
-        {task.status === "done" ? (
+        {isToggling ? (
+          <Spinner />
+        ) : task.status === "done" ? (
           <ClipboardCheck className="text-green-500" />
         ) : (
           <ClipboardPenLine />
