363ee2ef2e965b84c2a0d544adefb00140c9a50c
Ryan Luong <lumintriet@gmail.com>
2026-06-18 09:24:12 +0700

    feat(db/queries): simplify selectManyTasks error handling even more.

--- src/db/queries/select-many-tasks.server.ts
+++ src/db/queries/select-many-tasks.server.ts
@@ -1,6 +1,6 @@
 import { SQLiteError } from "bun:sqlite";
 
-import type { SQLiteBusyRecoveryErrorCode } from "@/db/types";
+import type { UnexpectedDatabaseErrorCode } from "@/db/types";
 import type { UnexpectedErrorCode } from "@/types/app-error";
 import type { Result } from "@/types/result";
 
@@ -8,7 +8,7 @@ import { type SelectedTask, taskTable } from "@/db/schema/tables/task";
 import { db } from "@/db";
 
 export async function selectManyTasks(): Promise<
-  Result<SelectedTask[], SQLiteBusyRecoveryErrorCode | UnexpectedErrorCode>
+  Result<SelectedTask[], UnexpectedDatabaseErrorCode | UnexpectedErrorCode>
 > {
   try {
     const tasks = await db.select().from(taskTable);
@@ -17,22 +17,13 @@ export async function selectManyTasks(): Promise<
       data: tasks,
     };
   } catch (error) {
-    if (error instanceof SQLiteError && error.errno === 261) {
-      return {
-        success: false,
-        error: {
-          code: "SQLITE_BUSY_RECOVERY_ERROR",
-          message:
-            "Another process is busy recovering the database file after a crash while this process is selecting tasks from the database.",
-          retryable: true,
-        },
-        context: { cause: error },
-      };
-    }
     return {
       success: false,
       error: {
-        code: "UNEXPECTED_ERROR",
+        code:
+          error instanceof SQLiteError
+            ? "UNEXPECTED_DATABASE_ERROR"
+            : "UNEXPECTED_ERROR",
         message:
           "An unexpected error occurred while selecting tasks from the database.",
         retryable: false,
--- src/db/types.ts
+++ src/db/types.ts
@@ -1,5 +1,1 @@
-export type SQLiteNoMemoryErrorCode = "SQLITE_NO_MEMORY_ERROR";
-
-export type SQLiteBusyRecoveryErrorCode = "SQLITE_BUSY_RECOVERY_ERROR";
-
-export type SQLiteCorruptIndexErrorCode = "SQLITE_CORRUPT_INDEX_ERROR";
+export type UnexpectedDatabaseErrorCode = "UNEXPECTED_DATABASE_ERROR";
