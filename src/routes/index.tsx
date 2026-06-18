bcfc8943c2eedcb8d72d17d3c6c79066dad2044a
Ryan Luong <lumintriet@gmail.com>
2026-06-18 10:01:40 +0700

    feat(ui): populate the home page.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,30 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { listTasks } from "@/features/task/operations/list-tasks";
+import { TaskList } from "@/features/task/components/list";
+
 export const Route = createFileRoute("/")({
+  loader: () => listTasks(),
   component: HomePage,
 });
 
 function HomePage() {
-  return <h1>Welcome Home!</h1>;
+  const listTasksResult = Route.useLoaderData();
+
+  if (!listTasksResult.success) {
+    return (
+      <div className="flex h-full flex-col items-center justify-center gap-y-3">
+        <h1 className="text-lg text-destructive">
+          500 - Internal server error
+        </h1>
+        <p>{listTasksResult.error.message}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex h-full flex-col items-center justify-center gap-y-3">
+      <TaskList tasks={listTasksResult.data} />
+    </div>
+  );
 }
