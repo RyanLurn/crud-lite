e3ac2b57b76aae0393002a39b78531dcfbf53c64
Ryan Luong <lumintriet@gmail.com>
2026-06-18 08:34:39 +0700

    feat(config/env): add SQLITE_MODE env var.

--- src/config/env.server.ts
+++ src/config/env.server.ts
@@ -3,6 +3,9 @@ import { z } from "zod";
 const envValidator = z.object({
   NODE_ENV: z.enum(["development", "testing", "staging", "production"]),
   SQLITE_FILE_PATH: z.string().min(1),
+  SQLITE_MODE: z
+    .enum(["ROLLBACK_JOURNAL", "WRITE-AHEAD_LOG"])
+    .default("ROLLBACK_JOURNAL"),
 });
 
 export const env = envValidator.parse(process.env);
