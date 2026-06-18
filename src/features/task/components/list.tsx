678293214f73fa42a7bbb1925a070f7514b1fa77
Ryan Luong <lumintriet@gmail.com>
2026-06-18 07:59:49 +0700

    feat(task/ui): define TaskList component.

---
+++ src/features/task/components/list.tsx
@@ -0,0 +1,21 @@
+import type { ComponentProps } from "react";
+
+import type { SelectedTask } from "@/db/schema/tables/task";
+import type { StrictOmit } from "@/types/helpers";
+
+import { TaskItem } from "@/features/task/components/item";
+import { ItemGroup } from "@/components/ui/item";
+
+interface TaskListProps extends ComponentProps<typeof ItemGroup> {
+  tasks: StrictOmit<SelectedTask, "createdAt" | "updatedAt">[];
+}
+
+export function TaskList({ tasks, ...props }: TaskListProps) {
+  return (
+    <ItemGroup {...props}>
+      {tasks.map((task) => (
+        <TaskItem key={task.id} task={task} />
+      ))}
+    </ItemGroup>
+  );
+}
