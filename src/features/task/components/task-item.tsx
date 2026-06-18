05a2a70194fd569d2490736d9b7b5dfb79b1879b
Ryan Luong <lumintriet@gmail.com>
2026-06-18 18:12:45 +0700

    feat(task/ui): add line through style to TaskItem component.

--- src/features/task/components/task-item.tsx
+++ src/features/task/components/task-item.tsx
@@ -5,6 +5,7 @@ import { ClipboardPenLine, ClipboardCheck } from "lucide-react";
 import type { SelectedTask } from "@/db/schema/tables/task";
 
 import { ItemContent, ItemMedia, ItemTitle, Item } from "@/components/ui/item";
+import { cn } from "@/lib/cn";
 
 interface TaskItemProps extends ComponentProps<typeof Item> {
   task: SelectedTask;
@@ -25,7 +26,13 @@ export function TaskItem({
         )}
       </ItemMedia>
       <ItemContent>
-        <ItemTitle>{task.name}</ItemTitle>
+        <ItemTitle
+          className={cn(
+            task.status === "done" ? "text-muted-foreground line-through" : ""
+          )}
+        >
+          {task.name}
+        </ItemTitle>
       </ItemContent>
     </Item>
   );
