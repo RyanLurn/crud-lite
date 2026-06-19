f5144a8ea789d0eef5bd8eb4f36770b05c799f6a
Ryan Luong <lumintriet@gmail.com>
2026-06-19 21:25:10 +0700

    feat(log): declare pino logger.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import handler, { createServerEntry } from "@tanstack/react-start/server-entry";
 
+import { logger } from "@/lib/pino/logger";
 import { env } from "@/config/env.server";
 
 // Making sure that the env module is loaded before most other modules.
-console.log(`[ENV] Server started in ${env.NODE_ENV} environment.`);
+logger.info(`[ENV] Server started in ${env.NODE_ENV} environment.`);
 
 export default createServerEntry({
   fetch(request) {
