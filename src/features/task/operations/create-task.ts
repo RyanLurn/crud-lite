2ed47a6e25c97e36b836a043ab9b24e1e41c9263
Ryan Luong <lumintriet@gmail.com>
2026-06-19 09:04:12 +0700

    feat(utils): declare HTTP_STATUS.

--- src/features/task/operations/create-task.ts
+++ src/features/task/operations/create-task.ts
@@ -6,10 +6,11 @@ import type { SerializableResult } from "@/types/serializable-result";
 import type { InternalServerErrorCode } from "@/types/app-error";
 
 import { insertTask } from "@/db/queries/insert-task.server";
+import { HTTP_STATUS } from "@/utils/http-status";
 
 export const createTaskValidator = z.object({
   name: z
-    .string()
+    .string("Task name is invalid.")
     .trim()
     .normalize("NFC")
     .min(1, "Task name is required.")
@@ -27,7 +28,10 @@ export const createTask = createServerFn({ method: "POST" })
       if (!insertTaskResult.success) {
         console.error(insertTaskResult);
 
-        setResponseStatus(500);
+        setResponseStatus(
+          HTTP_STATUS.INTERNAL_SERVER_ERROR.code,
+          HTTP_STATUS.INTERNAL_SERVER_ERROR.text
+        );
         return {
           success: false,
           error: {
