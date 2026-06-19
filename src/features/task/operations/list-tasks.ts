fb2d85f597638662ab90af284604837b10f2b62b
Ryan Luong <lumintriet@gmail.com>
2026-06-19 22:04:44 +0700

    feat(task/list): add logging to listTasks server function.

--- src/features/task/operations/list-tasks.ts
+++ src/features/task/operations/list-tasks.ts
@@ -1,44 +1,57 @@
 import { setResponseStatus } from "@tanstack/react-start/server";
 import { createServerFn } from "@tanstack/react-start";
+import { serializeError } from "serialize-error";
 
+import type { SuccessResult, ErrorResult, Result } from "@/types/result";
 import type { FallbackErrorCode } from "@/types/app-error";
-import type { Result } from "@/types/result";
 
 import { type SelectedTask, taskTable } from "@/db/schema/tables/task";
 import { formatErrorMessage } from "@/utils/format-error-message";
 import { HTTP_STATUS } from "@/utils/http-status";
+import { logger } from "@/lib/pino/logger";
 import { db } from "@/db";
 
 export const listTasks = createServerFn().handler(
   async (): Promise<Result<SelectedTask[], FallbackErrorCode>> => {
     try {
       const tasks = await db.select().from(taskTable);
-      return {
+
+      const successResult: SuccessResult<SelectedTask[]> = {
         success: true,
         data: tasks,
       };
+
+      logger.debug(
+        successResult,
+        `Selected ${tasks.length} task(s) from the database.`
+      );
+
+      return successResult;
     } catch (error) {
-      const message = formatErrorMessage({
-        action: "select tasks",
-        reason: "an unexpected error",
-      });
-
-      console.error(message);
-      console.error(error);
-
       setResponseStatus(
         HTTP_STATUS.INTERNAL_SERVER_ERROR.code,
         HTTP_STATUS.INTERNAL_SERVER_ERROR.text
       );
 
-      return {
+      const errorMessage = formatErrorMessage({
+        action: "select tasks",
+        reason: "an unexpected error",
+      });
+      const errorResult: ErrorResult<FallbackErrorCode> = {
         success: false,
         error: {
           code: "FALLBACK_ERROR",
-          message,
+          message: errorMessage,
           retryable: false,
         },
       };
+
+      logger.error(
+        { ...errorResult, cause: serializeError(error) },
+        errorMessage
+      );
+
+      return errorResult;
     }
   }
 );
